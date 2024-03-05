6598e84f1b98980fe508aa3f44763c03e190775f
Yahya Gilany <yahya.gilany@live.com>
2024-03-05 11:25:10 -0500

    staring point

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Button from './components/Button'
 
 function App() {
   return (
     <>
-      <div>
-          <img src={reactLogo} className="logo react" alt="React logo" />
-      </div>
-      <h1>IT3049C</h1>
-      <div className="card">
-        <p>
-        Edit <code>src/App.jsx</code> and save to update this view.
-        </p>
-      </div>
+      <h1>RPS in React</h1>
+      <Button /> {/* This line here is just to show you how a component can be nested here*/}
+      {/* Add the Rest of your React Components here */}
     </>
   )
 }
